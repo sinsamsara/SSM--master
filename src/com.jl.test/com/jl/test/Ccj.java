0da4b2b876f81b7784e0e17701bfbbebe379565e
jl <929582487@qq.com>
2020-04-21 16:22:56 +0800

    222222222222222222222222222222222

---
+++ src/com.jl.test/com/jl/test/Ccj.java
@@ -0,0 +1,13 @@
+package com.jl.test;
+
+/**
+ * @ClassName: [Ccj]
+ * @Description: []
+ * @Author: [jilun]
+ * @Time: [2020/4/21 16:21]
+ */
+public class Ccj {
+    public static void main(String[] args) {
+        System.out.println("成传军");
+    }
+}
